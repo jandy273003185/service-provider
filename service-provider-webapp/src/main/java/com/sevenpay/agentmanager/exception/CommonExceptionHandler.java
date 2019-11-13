6d380fa798edcfe527caffc16935035bdf714756
jiangzonglin <jiangzonglin@szgyzb.com>
2019-11-13 13:59:58 +0800

    完善接口

---
+++ service-provider-webapp/src/main/java/com/sevenpay/agentmanager/exception/CommonExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.sevenpay.agentmanager.exception;
+
+import com.sevenpay.agentmanager.pojo.ResultBean;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 统一异常处理
+ * User: JIANGZONGLIN
+ * Date: 2019/11/11
+ * Time: 21:36
+ */
+@ControllerAdvice
+public class CommonExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResultBean handleException(Exception e){
+        return new ResultBean("10001",e.getMessage());
+    }
+
+}
