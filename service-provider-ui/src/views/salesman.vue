e53412507d3637263509ed5719aa28a8cd599470
Holly <liuyuhua@szgyzb.com>
2019-11-20 14:22:19 +0800

    h

--- service-provider-ui/src/views/salesman.vue
+++ service-provider-ui/src/views/salesman.vue
@@ -4,17 +4,18 @@
     <div class="salesman">
       <div class="serachBox">
         <div class="search" @click="searchshop">
-          <img class="searchIcon" src="../assets/images/home/search.png" alt="">
+          <img class="searchIcon" src="../assets/images/home/search.png" alt />
           <div>搜索商户名称</div>
         </div>
       </div>
-      <div class="incomingBox"  @touchstart="toIncoming">
-        <div class="buttonAdd" >
-          <img src="../assets/images/home/add.png" alt="添加">
+      <div class="incomingBox" @touchstart="toIncoming">
+        <div class="buttonAdd">
+          <img src="../assets/images/home/add.png" alt="添加" />
           <p>商户进件</p>
         </div>
       </div>
-      <timeSelect v-if="islogin"></timeSelect>   <!--引入时间选择组件-->
+      <timeSelect v-if="islogin"></timeSelect>
+      <!--引入时间选择组件-->
       <div class="situation">
         <div class="titleBox">
           <div class="line"></div>
@@ -23,263 +24,307 @@
         </div>
 
         <ul>
-          <li v-for="(item, index) in statesList" :key="index" @click="toDetail(item.state,item.custId)">
+          <li
+            v-for="(item, index) in statesList"
+            :key="index"
+            @click="toDetail(item.state,item.custId)"
+          >
             <div>
               <span class="shopName">{{item.custName }}</span>
               <span class="time">{{ item.createTime }}</span>
             </div>
-            <span v-if="item.state=='00'" class=" state state_0">审核通过</span>
-            <span v-if="item.state=='01'"  class=" state state_1">待审核</span>
-            <span v-if="item.state=='04'" class=" state state_4">审核失败</span>
-            <span v-if="item.state=='05'" class=" state state_5">待完善</span>
+            <span v-if="item.state=='00'" class="state state_0">审核通过</span>
+            <span v-if="item.state=='01'" class="state state_1">待审核</span>
+            <span v-if="item.state=='04'" class="state state_4">审核失败</span>
+            <span v-if="item.state=='05'" class="state state_5">待完善</span>
           </li>
         </ul>
       </div>
     </div>
   </div>
-
 </template>
 
 <script>
-  import timeSelect from "../components/time-select";
-  import BaseHeader from "../components/baser-header.vue";
-  import {shopAuditInfo,login} from "../assets/api/interface";
-  import {mapMutations} from 'vuex';
-  import storage from '../assets/modeljs/storage.js';
-  import axios from "axios";
-  import http from "../assets/api/http";
-
+import timeSelect from "../components/time-select";
+import BaseHeader from "../components/baser-header.vue";
+import { shopAuditInfo, login } from "../assets/api/interface";
+import { mapMutations } from "vuex";
+import storage from "../assets/modeljs/storage.js";
+import axios from "axios";
+import http from "../assets/api/http";
 
 export default {
   name: "salesman",
-  components: { timeSelect,BaseHeader },
+  components: { timeSelect, BaseHeader },
   data() {
     return {
-      islogin:false,
-      openId:'',
-      roleId:'salesman',
-      statesList:[],
-      userId:''
-    }
+      islogin: false,
+      openId: "",
+      roleId: "salesman",
+      statesList: [],
+      userId: ""
+    };
   },
-  created(){
+  created() {
     this.$store.commit("setincoming", {});
     this.$store.commit("setPhotos", []);
-    this.$store.commit("setCheckedState", "");
-    this.firstLogin();
-
-    this.setRole(this.roleId);
-    this.setRoleId('3');
+    this.$store.commit("setCheckedState", ""); //snsapi_base
+    var code = this.getUrlParam("code");
+    if (!code) {
+       let REDIRECT_URI = encodeURIComponent("https://sp.qifenqian.com/wx/index.html");
+      window.location.href =
+        "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxce65746e62998dce&redirect_uri=" +
+        REDIRECT_URI +
+        "&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
+    } else {
+      let openId=this.getOpneId(code);
+      console.log("created"+openId);
+     /*  this.setRole(this.roleId);
+      this.setRoleId("3");
+      this.firstLogin(); */
+    }
   },
-  mounted() {
+  mounted() {},
 
-
-  },
-
-  methods:{
-
-    toIncoming(){//路由跳转到商户进件页面
+  methods: {
+    async getopenId(code){
+      let openId=await login.getOpenId();
+      console.log(openId);
+      return openId;
+    },
+    getUrlParam(name) {//获取code
+      var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+      var r = window.location.search.substr(1).match(reg);
+      if (r != null) return unescape(r[2]);
+      return null;
+    },
+    toIncoming() {
+      //路由跳转到商户进件页面
       console.log("商户进件");
-      this.$router.push('baseInfo');
+      this.$router.push("baseInfo");
     },
-    searchshop(){
-      this.$router.push('searchShop');
+    searchshop() {
+      this.$router.push("searchShop");
     },
 
-
-    async firstLogin(){//初次进入主页，传OpenId到后台，判断是否有绑定过账户
-        const getOpenId = await login.getOpenId();
-        this.openId = getOpenId.resultMsg;
-        this.setOpenID(this.openId);
-        console.log(this.openId);
-        if(this.openId){
-          const params = {
-            openId:this.openId,
-            roleId:this.roleId
-          };
-          const {data} = await login.firstLogin(params);
-          console.log(data);
-          if(data.resultCode=='0'){
-            this.$router.push('login');
-          }
-          if(data.resultCode=='1'){
-            localStorage.setItem('token',data.resultMsg.token);
-            axios.defaults.headers.common['token']= data.resultMsg.token;
-            this.setToken(data.resultMsg.token);
-            this.setUserId(data.resultMsg.userId);
-            storage.set("userId", data.resultMsg.userId);
-            console.log(storage.get('userId'));
-            this.islogin=true;
-            this.salesShopNew();
-          }
+    async firstLogin() {
+      //初次进入主页，传OpenId到后台，判断是否有绑定过账户
+      const getOpenId = await login.getOpenId();
+      this.openId = getOpenId.resultMsg;
+      this.setOpenID(this.openId);
+      console.log(this.openId);
+      if (this.openId) {
+        const params = {
+          openId: this.openId,
+          roleId: this.roleId
+        };
+        const { data } = await login.firstLogin(params);
+        console.log(data);
+        if (data.resultCode == "0") {
+          this.$router.push("login");
         }
-
+        if (data.resultCode == "1") {
+          localStorage.setItem("token", data.resultMsg.token);
+          axios.defaults.headers.common["token"] = data.resultMsg.token;
+          this.setToken(data.resultMsg.token);
+          this.setUserId(data.resultMsg.userId);
+          storage.set("userId", data.resultMsg.userId);
+          console.log(storage.get("userId"));
+          this.islogin = true;
+          this.salesShopNew();
+        }
+      }
     },
     //业务员主页下部分商户进件最新十条信息
 
-    async salesShopNew(){
-      let listInfo= await shopAuditInfo.shopAuditInfo({
-        userId:storage.get("userId"),
-        pageSize:'10',
-        pageNum:'1',
-        roleId:'3'
+    async salesShopNew() {
+      let listInfo = await shopAuditInfo.shopAuditInfo({
+        userId: storage.get("userId"),
+        pageSize: "10",
+        pageNum: "1",
+        roleId: "3"
       });
       console.log(storage.get("userId"));
       console.log(listInfo);
       this.statesList = listInfo.data.resultMsg.data;
       /*let total=listInfo.data.resultMsg.total;*/
-
     },
     //查看审核失败信息和审核成功信息
-  toDetail(state,custId){
-    if(state=='04'){
-      this.$router.push('whyFailed');
-    }
-    if(state=='00'){
-      this.$router.push('/audit/pass');
-    }
-    if (state == "05") {
-      this.$router.push({
-        name: "baseInfo",
-        params: { type: "corvidae", custId: custId }
-      });
-    }
-    this.setCustId(custId);
-  },
+    toDetail(state, custId) {
+      if (state == "04") {
+        this.$router.push("whyFailed");
+      }
+      if (state == "00") {
+        this.$router.push("/audit/pass");
+      }
+      if (state == "05") {
+        this.$router.push({
+          name: "baseInfo",
+          params: { type: "corvidae", custId: custId }
+        });
+      }
+      this.setCustId(custId);
+    },
 
-      ...mapMutations(['setRole','setRoleId','setUserId','setToken','setCustId','setOpenID'])
+    ...mapMutations([
+      "setRole",
+      "setRoleId",
+      "setUserId",
+      "setToken",
+      "setCustId",
+      "setOpenID"
+    ])
   }
 };
 </script>
 <style scoped lang="stylus" ref="stylesheet/stylus">
-  @import "../style/common/base.styl";
-  .salesman{
+@import '../style/common/base.styl';
+
+.salesman {
+  width: 100%;
+  height: 100%;
+  background-color: #EEEEEE;
+
+  .serachBox {
     width: 100%;
-    height: 100%;
-    background-color:#EEEEEE;
+    height: vw(92);
+    background-color: #699dd7;
+    display: flex;
+    align-items: center;
 
-    .serachBox{
-      width: 100%;
-      height: vw(92);
-      background-color: #699dd7;
-      display:flex;
-      align-items:center;
+    .search {
+      width: vw(580);
+      height: vw(58);
+      margin-left: vw(34);
+      background-color: #eeeeee;
+      display: flex;
+      align-items: center;
+      justifu-content: center;
+      border-radius: vw(10);
 
-      .search{
-        width: vw(580);
-        height: vw(58);
-        margin-left: vw(34) ;
-        background-color:#eeeeee
-        display:flex;
-        align-items:center;
-        justifu-content:center;
-        border-radius:vw(10);
+      .searchIcon {
+        width: vw(50);
+        height: vw(50);
+      }
 
-        .searchIcon{
-          width: vw(50);
-          height: vw(50);
-        }
-
-        div{
-          height: vw(50);
-          font-size: vw(32);
-          color: #999999;
-        }
+      div {
+        height: vw(50);
+        font-size: vw(32);
+        color: #999999;
       }
     }
-   .incomingBox{
-     width: vw(690);
-     height: vw(80);
-     margin: vw(36) auto;
-     border:vw(1) dashed #699dd7;
+  }
 
-     .buttonAdd{
-       width: vw(220);
-       height: vw(40);
-       margin: vw(20) auto;
-       /*border: 1px solid red;*/
-       display: flex;
-       align-items: center;
-       justify-content: space-between;
+  .incomingBox {
+    width: vw(690);
+    height: vw(80);
+    margin: vw(36) auto;
+    border: vw(1) dashed #699dd7;
 
-       img{
-         width: vw(40);
-         height: vw(40);
-       }
+    .buttonAdd {
+      width: vw(220);
+      height: vw(40);
+      margin: vw(20) auto;
+      /* border: 1px solid red; */
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
 
-       p{
-         height: vw(40);
-         line-height: vw(40);
-         font-size: vw(40);
-         color: #699dd7;
-       }
-     }
-   }
-   .situation{
-     width: 100%;
-     height: vw(600);
-     box-sizing border-box;
-      .titleBox{
+      img {
+        width: vw(40);
+        height: vw(40);
+      }
+
+      p {
+        height: vw(40);
+        line-height: vw(40);
+        font-size: vw(40);
+        color: #699dd7;
+      }
+    }
+  }
+
+  .situation {
+    width: 100%;
+    height: vw(600);
+    box-sizing: border-box;
+
+    .titleBox {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      .line {
+        height: vw(2);
+        width: vw(100);
+        background-color: #c0c0c0;
+        flex-grow: 1;
+      }
+
+      .titleBar {
+        height: vw(30);
+        line-height: vw(30);
+        text-align: center;
+        color: #333333;
+        font-size: vw(30);
+      }
+    }
+
+    ul {
+      width: 100%;
+      margin: auto;
+      background-color: #fff;
+
+      li {
+        width: vw(680);
+        height: vw(94);
+        margin: 0 auto;
         display: flex;
         justify-content: space-between;
         align-items: center;
-        .line{
-          height: vw(2);
-          width: vw(100);
-          background-color: #c0c0c0;
-          flex-grow: 1;
-        }
-        .titleBar{
-          height: vw(30);
-          line-height: vw(30);
-          text-align: center;
+        border-bottom: vw(1) solid #EEEEEE;
+
+        div {
           color: #333333;
-          font-size: vw(30);
+          height: 100%;
+          margin: 0;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-around;
+          align-items: flex-start;
+
+          .shopName {
+            color: #030303;
+            font-size: vw(32);
+          }
+
+          .time {
+            color: #969696;
+            font-size: vw(22);
+          }
+        }
+
+        .state {
+          font-size: vw(32);
+        }
+
+        .state_0 {
+          color: #4dff1a;
+        }
+
+        .state_1 {
+          color: #aeaeae;
+        }
+
+        .state_4 {
+          color: #ff495d;
+        }
+
+        .state_5 {
+          color: #544eec;
         }
       }
-
-  ul{
-    width: 100%;
-    margin:auto;
-    background-color: #fff;
-
-  li{
-    width: vw(680);
-    height: vw(94);
-    margin: 0 auto;
-    display: flex;
-    justify-content:space-between;
-    align-items: center;
-    border-bottom: vw(1) solid #EEEEEE;
-
-  div{
-    color: #333333;
-    height: 100%;
-    margin: 0;
-    display: flex;
-    flex-direction: column;
-    justify-content:space-around;
-    align-items: flex-start;
-  .shopName{
-    color: #030303;
-    font-size: vw(32);
+    }
   }
-  .time{
-    color: #969696;
-    font-size: vw(22);
-  }
-  }
-  .state{
-    font-size: vw(32);
-  }
-  .state_0{color: #4dff1a;}
-  .state_1{color: #aeaeae;}
-  .state_4{color: #ff495d;}
-  .state_5{color: #544eec;}
-  }
-  }
-
-  }
-
-  }
+}
 </style>
