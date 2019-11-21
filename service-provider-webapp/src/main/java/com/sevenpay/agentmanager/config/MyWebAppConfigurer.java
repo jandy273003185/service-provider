aa942a30f970718ca9c62529e70ed246e797254d
jiangzonglin <jiangzonglin@szgyzb.com>
2019-11-21 10:27:57 +0800

    1

--- service-provider-webapp/src/main/java/com/sevenpay/agentmanager/config/MyWebAppConfigurer.java
+++ service-provider-webapp/src/main/java/com/sevenpay/agentmanager/config/MyWebAppConfigurer.java
@@ -21,6 +21,7 @@ public class MyWebAppConfigurer implements WebMvcConfigurer {
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler(relativePaths).addResourceLocations("file:/"+absolutePaths);
+        registry.addResourceHandler(relativePaths)
+                .addResourceLocations("file://"+absolutePaths);
     }
 }
