d112bc5a4abde271afe570750b8b7805c4a1b975
jiangzonglin <jiangzonglin@szgyzb.com>
2019-11-27 10:36:25 +0800

    jiangzonglin

--- service-provider-webapp/src/main/java/com/sevenpay/agentmanager/controller/CommonController.java
+++ service-provider-webapp/src/main/java/com/sevenpay/agentmanager/controller/CommonController.java
@@ -1,8 +1,10 @@
 package com.sevenpay.agentmanager.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.qifenqian.app.bean.Bank;
 import com.qifenqian.app.bean.TbBankProvincesInfoBean;
 import com.qifenqian.app.bean.TbProvincesInfoBean;
+import com.qifenqian.app.common.BankInfoService;
 import com.qifenqian.app.customer.MerchantInfoService;
 import com.sevenpay.agentmanager.pojo.ResultBean;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +23,9 @@ public class CommonController {
 
     @Reference
     private MerchantInfoService merchantInfoService;
+
+    @Reference
+    private BankInfoService bankInfoService;
 
     /**
      * 省市县三级下拉
@@ -43,4 +48,33 @@ public class CommonController {
         List<TbBankProvincesInfoBean> bankProvinces = merchantInfoService.getBankProvinces(tbBankProvincesInfoBean);
         return new ResultBean<List<TbBankProvincesInfoBean>>("1",bankProvinces);
     }
+
+    /**
+     * 查询总行（带模糊）
+     * @param bank
+     * @return
+     */
+    @RequestMapping("bankHeadOffice")
+    public ResultBean bankHeadOffice(Bank bank){
+        List<Bank> banks = bankInfoService.selectBanks(bank);
+        if (banks.size() > 0) {
+            return new ResultBean("1",banks);
+        }
+        return new ResultBean("0");
+    }
+
+    /**
+     * 查询支行（带模糊）
+     * @param bank
+     * @return
+     */
+    @RequestMapping("bankBranch")
+    public ResultBean bankBranch(Bank bank){
+        List<Bank> banks = bankInfoService.selectBranchBanks(bank);
+        if (banks.size() > 0) {
+            return new ResultBean("1",banks);
+        }
+        return new ResultBean("0");
+    }
+
 }
