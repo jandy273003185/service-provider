8792d47652e535a93c2f9722f5bbbe49f0a5933d
jiangzonglin <jiangzonglin@szgyzb.com>
2019-11-27 14:37:36 +0800

    jiangzonglin

--- service-provider-webapp/src/main/java/com/sevenpay/agentmanager/controller/CommonController.java
+++ service-provider-webapp/src/main/java/com/sevenpay/agentmanager/controller/CommonController.java
@@ -4,12 +4,20 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.qifenqian.app.bean.Bank;
 import com.qifenqian.app.bean.TbBankProvincesInfoBean;
 import com.qifenqian.app.bean.TbProvincesInfoBean;
+import com.qifenqian.app.bean.dto.MessageDTO;
+import com.qifenqian.app.bean.dto.MessageTemplate;
 import com.qifenqian.app.common.BankInfoService;
+import com.qifenqian.app.common.MessageManager;
 import com.qifenqian.app.customer.MerchantInfoService;
 import com.sevenpay.agentmanager.pojo.ResultBean;
+import com.sevenpay.agentmanager.utils.GenSN;
+import com.sevenpay.agentmanager.utils.verfycode.ToolsUtil;
+import com.sevenpay.agentmanager.utils.verfycode.VerifyInfoConstant;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -26,6 +34,10 @@ public class CommonController {
 
     @Reference
     private BankInfoService bankInfoService;
+
+    @Reference
+    private MessageManager messageManager;
+
 
     /**
      * 省市县三级下拉
@@ -74,7 +86,35 @@ public class CommonController {
         if (banks.size() > 0) {
             return new ResultBean("1",banks);
         }
-        return new ResultBean("0");
+        Bank bankHeadOffice = bankInfoService.selectBankByBankCode(bank.getBankCode());
+        return new ResultBean("0",bankHeadOffice);
     }
+
+    /**
+     * 发送短信验证码接口
+     * @param request
+     * @param messageDTO
+     * @return
+     */
+    @RequestMapping("verifyCode")
+    public ResultBean verifyCode(HttpServletRequest request, MessageDTO messageDTO){
+        Date sendTime = (Date)request.getSession().getAttribute("REGISTER_VERIFY_SEND_TIME");
+
+        //短信发送间隔必须超过55s
+        if(null != sendTime && ToolsUtil.calTimeSec(new Date(), sendTime) < 55)
+        {
+            return new ResultBean("0","验证码发送间隔为60s");
+        }
+
+        //生成验证码
+        String smsVerifyCode = GenSN.getRandomNum(6);
+        request.getSession().setAttribute(VerifyInfoConstant.REGISTER_VERIFY_CODE, smsVerifyCode);
+        request.getSession().setAttribute(VerifyInfoConstant.REGISTER_VERIFY_ACC, messageDTO.getAddressee());
+        request.getSession().setAttribute(VerifyInfoConstant.REGISTER_VERIFY_SEND_TIME, new Date());
+        messageDTO.setCode(smsVerifyCode);
+        MessageDTO m = messageManager.sendMobileCode(messageDTO);
+        return new ResultBean("1");
+    }
+
 
 }
