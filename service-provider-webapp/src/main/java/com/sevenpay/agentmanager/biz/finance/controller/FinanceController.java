b8b0d03f0699042a723a291ef7d8f8f02d8ad6d8
hehaijun <hehaijun@qq.com>
2019-12-31 18:53:06 +0800

    财务员

---
+++ service-provider-webapp/src/main/java/com/sevenpay/agentmanager/biz/finance/controller/FinanceController.java
@@ -0,0 +1,103 @@
+package com.sevenpay.agentmanager.biz.finance.controller;
+
+
+import com.qifenqian.app.bean.customer.FinanceInfo;
+import com.sevenpay.agentmanager.biz.finance.service.FinanceServiceImpl;
+import com.sevenpay.agentmanager.core.bean.ResultData;
+import com.sevenpay.agentmanager.core.controller.AbstractBaseController;
+import com.sevenpay.agentmanager.core.exception.BizException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 财务员
+ */
+@RestController
+@RequestMapping("finance")
+public class FinanceController extends AbstractBaseController {
+
+
+    @Autowired
+    FinanceServiceImpl financeService;
+
+
+
+    /**
+     * 服务商添加财务员
+     * @param financeInfo
+     * @return
+     */
+    @RequestMapping(value = "addFinance")
+    public ResultData addFinance(FinanceInfo financeInfo){
+
+        if(null == financeInfo.getFinanceMobile()){
+            throw new BizException("手机不能为空~");
+        }
+        if(null == financeInfo.getFinanceName()){
+            throw new BizException("姓名不能为空~");
+        }
+        if(null == financeInfo.getCustId()){
+            throw new BizException("服务商id不能为空~");
+        }
+
+        return financeService.addFinance(financeInfo);
+
+    }
+
+
+    /**
+     * 财务员列表查询（手机或商户编号）
+     * @param custId
+     * @return
+     */
+    @RequestMapping(value = "getFinanceList")
+    public ResultData getFinanceList(String custId){
+
+        if(custId.equals("")||null == custId){
+            throw new BizException("服务商id不能为空~");
+        }
+
+        FinanceInfo financeInfo = new FinanceInfo();
+
+
+        return financeService.getFinanceList(financeInfo);
+    }
+
+
+
+    /**
+     * 财务员修改密码、重置密码、退款密码
+     * @param financeId
+     * @param refundPw
+     * @param refundNewPw
+     * @param loginPw
+     * @param loginNewPw
+     * @return
+     */
+    @RequestMapping(value = "updateFinancePw")
+    public ResultData updateFinancePw(String financeId, String refundPw, String refundNewPw, String loginPw, String loginNewPw){
+
+        return financeService.updateFinancePw(financeId,refundPw,refundNewPw,loginPw,loginNewPw);
+
+    }
+
+
+
+    /**
+     * 修改状态
+     * @param financeInfo
+     * @return
+     */
+    @RequestMapping(value = "updateFinance")
+    public ResultData updateFinance(FinanceInfo financeInfo){
+
+        return financeService.updateFinance(financeInfo);
+
+    }
+
+
+
+
+
+}
---
+++ service-provider-webapp/src/main/java/com/sevenpay/agentmanager/biz/finance/service/FinanceServiceImpl.java
@@ -0,0 +1,124 @@
+package com.sevenpay.agentmanager.biz.finance.service;
+
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.qifenqian.app.bean.customer.FinanceInfo;
+import com.qifenqian.app.enterprise.finance.FinanceManageService;
+import com.sevenpay.agentmanager.core.bean.ResultData;
+import com.sevenpay.agentmanager.core.exception.BizException;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+
+@Service
+public class FinanceServiceImpl {
+
+
+    @Reference
+    FinanceManageService financeManageService;
+
+
+    /**
+     * 添加财务员
+     *
+     * @param financeInfo
+     * @return
+     */
+    public ResultData addFinance(FinanceInfo financeInfo) {
+
+        /* 默认登陆密码 */
+        financeInfo.setLoginPw("123456");
+
+        /* 默认生效 */
+        financeInfo.setStatus("1");
+
+        /* 没有退款权限 */
+        financeInfo.setRefundAuth("0");
+
+        /* 查询权限 1-查询全部数据 */
+        financeInfo.setQueryAuth("1");
+
+        /* 创建时间 */
+        financeInfo.setCreateTime(new Date());
+
+        /* 创建人 */
+        financeInfo.setCreateId(financeInfo.getCustId());
+
+        try {
+            boolean result = financeManageService.addFinance(financeInfo);
+            if (result) {
+                return ResultData.success();
+            }
+        } catch (Exception e) {
+            throw new BizException("DUBBO接口异常" + e.getMessage());
+        }
+
+        return ResultData.error("添加出错");
+    }
+
+
+
+    /**
+     * 财务员修改密码、重置密码，退款密码
+     * @param financeId
+     * @param refundPw
+     * @param refundNewPw
+     * @param loginPw
+     * @param loginNewPw
+     * @return
+     */
+    public ResultData updateFinancePw(String financeId,
+                                      String refundPw,
+                                      String refundNewPw,
+                                      String loginPw,
+                                      String loginNewPw){
+
+        try {
+            boolean result = financeManageService.updateFinancePw(financeId, refundPw, refundNewPw, loginPw, loginNewPw);
+            if (result) {
+                return ResultData.success();
+            }
+        } catch (Exception e) {
+            throw new BizException("DUBBO接口异常" + e.getMessage());
+        }
+
+        return ResultData.error("修改出错");
+    }
+
+
+    /**
+     * 修改状态
+     * @param financeInfo
+     * @return
+     */
+    public ResultData updateFinance(FinanceInfo financeInfo){
+
+        try {
+            boolean result = financeManageService.updateFinance(financeInfo);
+            if (result) {
+                return ResultData.success();
+            }
+        } catch (Exception e) {
+            throw new BizException("DUBBO接口异常" + e.getMessage());
+        }
+        return ResultData.error("修改出错");
+    }
+
+
+
+    /**
+     * 财务员列表查询（手机或商户编号）
+     * @param financeInfo
+     * @return
+     */
+    public ResultData getFinanceList(FinanceInfo financeInfo){
+
+        List<FinanceInfo> financeInfoList = financeManageService.getFinanceList(financeInfo);
+
+        return ResultData.success(financeInfoList);
+    }
+
+
+}
