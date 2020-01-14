c99dff35132673697b01d8f66005158702c6ef11
zhangjiangang <zhangjiangang@szgyzb.com>
2020-01-14 17:32:39 +0800

    隐藏分润权限的状态

--- service-provider-ui/vue.config.js
+++ service-provider-ui/vue.config.js
@@ -26,9 +26,9 @@ module.exports = {
             '/': {
                 ws: false,
                 /* 目标代理服务器地址 */
-                // target: 'https://sp-uat.qifenqian.com',
+                target: 'https://sp-uat.qifenqian.com',
 
-                target: 'http://192.168.1.141:8083', //何老板
+                // target: 'http://192.168.1.141:8083', //何老板
                 // target: 'http://192.168.1.131:9999',
                 // target: 'http://192.168.1.192:8082', //李老板
 
